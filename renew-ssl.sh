a50385ccbeefc9d4c72be8f6ebadc2a176d7689c
pcking60 <pcking60@gmail.com>
2025-05-14 11:02:07 +0700

    chore: remove outdated SSL renewal script and update certificate files
    Deleted the `renew-ssl.sh` script as it is no longer needed. Added new certificate files (`RootCA.crt`, `privkey.pem`, `fullchain.pem`) and removed outdated certificate files (`private_key.pem`, `star_phuongchau_com_cert.pem`, `fullchain.crt`). Updated the `chain.pem` file with the latest certificate chain.

--- renew-ssl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Thử gia hạn chứng chỉ
-docker compose run --rm certbot renew
-
-# Nếu gia hạn thành công, reload NGINX để áp dụng chứng chỉ mới
-if [ $? -eq 0 ]; then
-    docker compose -f ../docker-compose.yml exec nginx nginx -s reload
-fi
